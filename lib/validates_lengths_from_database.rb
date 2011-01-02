eb225416104d8643ae3e2ca5606dc65166340d62
Ben Simpson <thehoagie@gmail.com>
2011-01-02 13:42:10 -0500

    Added new validation for the decimal and integer datatypes. The length of the integer value depends on the database implementation (MySQL is 2**(4*8), or 4 bytes). Because this is database specific, I am specifically checking for the MySQL adapter before adding this validation

--- lib/validates_lengths_from_database.rb
+++ lib/validates_lengths_from_database.rb
@@ -11,27 +11,30 @@ module ValidatesLengthsFromDatabase
     def validates_lengths_from_database(options = {})
       options.symbolize_keys!
 
-      raise ArgumentError, "The :only option to validates_lengths_from_database must be an array." if options[:only] and !options[:only].is_a?(Array)
-      raise ArgumentError, "The :except option to validates_lengths_from_database must be an array." if options[:except] and !options[:except].is_a?(Array)
-
       if options[:only]
-        columns_to_validate = options[:only].map(&:to_s)
+        columns_to_validate = Array(options[:only]).map(&:to_s)
       else
         columns_to_validate = column_names.map(&:to_s)
-        columns_to_validate -= options[:except].map(&:to_s) if options[:except]
+        columns_to_validate -= Array(options[:except]).map(&:to_s) if options[:except]
       end
 
       columns_to_validate.each do |column|
         column_schema = columns.find {|c| c.name == column }
         next if column_schema.nil?
-        next if ![:string, :text].include?(column_schema.type)
-        next if column_schema.limit.nil?
+        next if column_schema.name[/id$/]
+        next if ![:string, :text, :decimal, :integer].include?(column_schema.type)
 
         class_eval do
-          validates_length_of column, :maximum => column_schema.limit, :allow_blank => true
-        end
+          validates_length_of column, :maximum => column_schema.limit, :allow_blank => true if column_schema.limit.present? and [:string, :text].include?(column_schema.type)
+          if column_schema.type == :decimal
+            before_validation {|record| errors.add(column, "Precision must have a maximum length of #{column_schema.precision}") if record[column].to_s.gsub('.','').length > column_schema.precision }
+            before_validation {|record| errors.add(column, "Scale must have a maximum length of #{column_schema.scale}") if a = record[column].to_s.split('.')[1] and a.length > column_schema.scale }
+          end
+          if column_schema.type == :integer and Testing.configurations[Rails.env]['adapter'][/mysql/]
+            validates_numericality_of column, :greater_than_or_equal_to => -2**(column_schema.limit*8) / 2, :less_than => (2**(column_schema.limit*8) / 2), :allow_blank => true
+          end
+        end 
       end
-
       nil
     end
 
